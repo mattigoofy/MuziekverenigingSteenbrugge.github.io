53720d80f786525246b671ea73df8de84c4d73b1
Mathias <159270053+mattigoofy@users.noreply.github.com>
2026-09-12 15:30:44 +0200

    PWA support

---
+++ LedenActiviteiten/sw.js
@@ -0,0 +1,67 @@
+var GHPATH = '/LedenActiviteiten';
+var APP_PREFIX = 'gppwa_';
+var VERSION = 'version_001';
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/manifest.webmanifest`,
+  `${GHPATH}/styles/index.css`,
+  `${GHPATH}/styles/algemeen.css`,
+  `${GHPATH}/styles/containers.css`,
+  `${GHPATH}/styles/login.css`,
+  `${GHPATH}/styles/main.css`,
+  `${GHPATH}/styles/events.css`,
+  `${GHPATH}/styles/info-events.css`,
+  `${GHPATH}/styles/mobile.css`,
+  `${GHPATH}/script/main.js`,
+  `${GHPATH}/script/auth.js`,
+  `${GHPATH}/script/handlers.js`,
+  `${GHPATH}/script/events.js`,
+  `${GHPATH}/script/loggedIn.js`,
+  `${GHPATH}/script/firebase.js`,
+  `${GHPATH}/script/state.js`,
+  `${GHPATH}/img/icon.png`,
+  `${GHPATH}/sw.js`
+]
+
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { 
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {       
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if(cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
